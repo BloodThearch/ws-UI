6ae1f4f61634613c7c7fa2fabc4e6dd00a2dcdd0
devanshu <devanshukumar45@gmail.com>
2021-07-31 06:59:26 +0530

    finalized

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,32 @@
 import 'package:flutter/material.dart';
-
-import 'views/home.dart';
+import 'package:ws_ui/utils/wrapper.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      home: Wrapper(),
     );
   }
 }
